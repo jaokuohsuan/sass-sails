9bd3a7ab05ef4f9e66021c97d50768c223418ea7
funnyjao <funnyjao@gmail.com>
2014-06-19 02:25:16 +0800

    add UI-Bootstrap

--- angular/app/js/app.js
+++ angular/app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ui.router',
+  'ui.bootstrap',
   'ngSails', // angular-sails
   'myApp.filters',
   'myApp.services',
