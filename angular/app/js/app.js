51dca0a5d4be488246d03ecf497a81d0ccd2491f
funnyjao <funnyjao@gmail.com>
2014-06-19 01:17:09 +0800

    transfer to ui-router

--- angular/app/js/app.js
+++ angular/app/js/app.js
@@ -3,15 +3,41 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'ngSails', // angular-sails
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+])
+.config(['$stateProvider', '$urlRouterProvider',function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+	$stateProvider
+		.state('home',{
+			url:'/view1',
+			views:{
+				'view.main':{
+					templateUrl: 'partials/partial1.html',
+					controller: 'MyCtrl1'
+				}
+			}
+
+		})
+		.state('home2',{
+			url:'/view2',
+			views:{
+				'view.main':{
+					templateUrl: 'partials/partial2.html',
+					controller: 'MyCtrl2'
+				}
+			}
+
+		})
+
+	
 }]);
+// .config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+//   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
